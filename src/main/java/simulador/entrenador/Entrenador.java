c756b5a640e19db0a79a66797d3aa17380622acc
Juan Cepeda <jualendrocep@gmail.com>
2024-10-28 18:37:57 -0500

    Se adelanta la clase entrenador

--- src/main/java/simulador/entrenador/Entrenador.java
+++ src/main/java/simulador/entrenador/Entrenador.java
@@ -1,5 +1,23 @@
 package simulador.entrenador;
+import java.util.LinkedList;
+import java.util.List;
+import simulador.pokemon.Pokemon;
 
 public class Entrenador {
-    // TODO: Aquí va tu código
+    private final String nombre;
+    private List<Pokemon> pokemones;
+    public static Pokemon pokemon;
+    
+    
+    public Entrenador(String nombre){
+        this.nombre=nombre;
+        this.pokemones=new LinkedList<>();
+    }
+    public void agregarPokemon(Pokemon pokemon){
+        if(pokemones.size()<6){
+            pokemones.add(pokemon);
+        }
+    }
+    
+    
 }
